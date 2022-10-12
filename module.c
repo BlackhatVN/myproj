b455878a043cbbb28b2fddd8d4fb307d6b80ec46
Vivekanand <113105286+BlackhatVN@users.noreply.github.com>
2022-10-12 19:15:09 +0530

    Add files via upload

---
+++ module.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int mod();
+int main()
+{
+    int a,b,c;
+    printf("input numbers");
+    scanf("%d%d",&a,&b);
+    c=a%b;
+    printf("remainder=%d",c);
+    getch();
+
+}
+int mod()
+{
+    int a,b,c;
+    printf("remainder=%d");
+
+return 0;
+}
