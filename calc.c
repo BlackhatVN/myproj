e175d172e5ca69bd3242498688b42e293d022a6e
Vivekanand <113105286+BlackhatVN@users.noreply.github.com>
2022-10-13 14:29:29 +0530

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,55 @@
+\\its vn babe
+\\calculator
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+     double num1 = 0.0;
+     double num2 = 0.0;
+     char   opera ;
+     double result = 0.0;
+
+     printf("Usage: numa /*+- numb\n");
+     while (1)
+     {
+         result = 0.0;
+         printf("please enter: \n");
+         scanf ("%lf %c %lf", &num1, &opera, &num2);
+
+         switch(opera)
+         {
+             case '+':
+                 result = num1 + num2;
+                 break;
+
+             case '-':
+                 result = num1 - num2;
+                 break;
+
+             case '*':
+                 result = num1 * num2;
+                 break;
+
+             case '/':
+                 if (num2 == 0)
+                 {
+                      printf("error: numb = 0!\n");
+                 }
+                 else
+                 {
+                      result = num1 / num2;
+                 }
+                 break;
+
+             default :
+                 break;
+         }//switch()
+
+         printf("=%lf\n", result);
+     }//while(1)
+
+     exit(0);
+}
